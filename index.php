77147906c301e3e024196743d1d66599d0e337b2
Adey <adeynugraha92@gmail.com>
2020-06-04 17:58:45 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -7,5 +7,9 @@
 </head>
 <body>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam, eius.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
